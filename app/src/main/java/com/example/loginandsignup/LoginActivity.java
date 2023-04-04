d9d851221826fba0e6da83a246800597509c8c9e
Leanh <datletb789@gmail.com>
2023-04-05 00:12:04 +0700

    homepage

--- app/src/main/java/com/example/loginandsignup/LoginActivity.java
+++ app/src/main/java/com/example/loginandsignup/LoginActivity.java
@@ -49,7 +49,7 @@ public class LoginActivity extends AppCompatActivity {
         btnsignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(),SignupActivity.class);
                 startActivity(intent);
             }
         });
